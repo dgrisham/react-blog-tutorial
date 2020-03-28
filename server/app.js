17c480f2f31314db7f5d261f2eefb4d3ebedb0c7
dgrisham <dmgrisham@gmail.com>
2020-03-28 08:03:55 -0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const errorHandler = require('errorhandler');
+const mongoose = require('mongoose');
+
+mongoose.promise = global.Promise;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'grish&friends', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+if (isProduction) {
+  app.use(errorHandler());
+}
+
+mongoose.connect('mongodb://localhost/grishblog');
+mongoose.set('debug', true);
+
+// add models
+require('./models/Articles');
+// add routes
+// app.use(require('./routes'));
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+const server = app.listen(8000, () => console.log('Server started on http://localhost:8000'));
